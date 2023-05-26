9aade5571c8b48a2cfb0deeda873b5ae25e64a2b
Kayed <alkayedm457@gmail.com>
2023-05-26 19:11:32 +0300

    server  file is modified

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*
 This is the starting point for the app 
 */
-const http = require('http');
-const app = require('./app');
+import { createServer } from 'http';
+import app from './app';
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+const server = createServer(app);
 server.listen(port,() => {
     console.log (`Started on port${port}`);
 });
