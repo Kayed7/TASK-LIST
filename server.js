63d1250ca90c1a56ceaaaefefc0433e6dae054be
Kayed <alkayedm457@gmail.com>
2023-05-26 19:05:34 +0300

    server file added

---
+++ server.js
@@ -0,0 +1,10 @@
+/*
+This is the starting point for the app 
+*/
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port,() => {
+    console.log (`Started on port${port}`);
+});
